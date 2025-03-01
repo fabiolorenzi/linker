762db455a9cc2c76c370fb2f853feb0787508af8
fabiolorenzi <fabiolorenzi13975@gmail.com>
2025-03-01 07:58:36 +0000

    reset changes

--- script.js
+++
@@ -1,7 +0,0 @@
-const target = new URLSearchParams(window.location.search).get("target") || null;
-
-if (target == "spotify") {
-    window.location.replace("https://open.spotify.com/artist/76kA5l0oToh6QDSlMxLpwD");
-} else if (target == "the-worst-side-ep") {
-    window.location.replace("https://open.spotify.com/album/6gvl2NSdPH4xzCUujK7MeU?si=VPl_Iqr7RQSW_uG7K_Uyhg");
-};
